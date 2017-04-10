fd016c3151a0de0a8269ff29672f44d79c7518f5
hairslinky <hello@lavitaglam.com>
2017-04-09 23:42:09 -0400

    initial release

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+
+//var https = require('https');
+var PORT = process.env.PORT || 3000;
+var app = express();
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(function (req, res, next){
+    console.log("HTTP request", req.method, req.url, req.body);
+    return next();
+});
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      //respond with 200
+      res.sendStatus(200);
+    }
+    else {
+    //move on
+      next();
+    }
+});
+
+
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'hello@lavitaglam.com',
+        pass: 'Invictusjan93'
+    }
+});
+
+app.post('/api/email/', function(req, res, next){
+    console.log(req.body);
+    // setup email data with unicode symbols
+    var result = '';
+    Object.keys(req.body).forEach(function(key){result += key + ": " + req.body[key] + "\n"});
+    let mailOptions = {
+        //from: '"Joshua " <joshua.aaaagreenslade6@gmail.com>', // sender address
+        to: 'hello@lavitaglam.com', // list of receivers
+        subject: 'New Hairslinkey Order', // Subject line
+        text: result//'I figured out how to send an email from a server and it is freeeeee!!!!!' // plain text body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
+res.end("yo yo yo")
+});
+
+
+app.use(function (req, res, next){
+    console.log("HTTP Response", res.statusCode);
+});
+
+
+app.listen(PORT, function () {
+  console.log('App listening on port 3000');
+});
---
+++ java.js
@@ -0,0 +1,44 @@
+/*jshint esversion: 6*/
+
+var view = (function(){
+//    "use strict";
+
+    
+
+    document.getElementById("blondebag").onkeyup = function(e){
+    	
+        document.getElementById("blondebagtotal").innerHTML = "$" + document.getElementById("blondebag").value * 28
+    }
+
+
+document.getElementById("brunettebag").onkeyup = function(e){
+    	
+        document.getElementById("brunettebagtotal").innerHTML = "$" + document.getElementById("brunettebag").value * 28
+    }
+
+    
+    document.getElementById('submit').onclick = function(e){
+    	var result={blondebag:document.getElementById("blondebag").value, brunettebag: document.getElementById("brunettebag").value}
+    	var method = "POST";
+        var url = /*"https://hairslinkey.herokuapp.com*/"http://localhost:3000/api/email/";
+        var body = JSON.stringify(result);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE){
+                console.log(this.status)
+                console.log("good")
+            }
+        };
+        xhr.open(method, url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(body);
+    }
+
+
+    var view = {};
+
+
+
+    return view;
+    
+}());
